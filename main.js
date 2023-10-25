f99d5c3ad7ba917685298bf398e9e911684ef5d7
FeddeARG <federicoanaranjo@gmail.com>
2023-10-24 23:41:54 -0300

    JsV1.1

--- main.js
+++ main.js
@@ -7,150 +7,139 @@ class Item {
     this.existencias = existencias;
   }
 }
-
 const item = [
   new Item(1000, "monitor", "periferico", 1000, 20),
   new Item(1001, "teclado", "periferico", 150, 15),
-  new Item(1002, "mouse", "periferico", 200, 10),
+  new Item(1002, "ratón", "periferico", 200, 10),
   new Item(1003, "windows11", "software", 150, 200),
   new Item(1004, "excel", "software", 50, 300),
-  new Item(1005, "adobe pack", "software", 100, 70),
+  new Item(1005, "paquete de adobe", "software", 100, 70),
   new Item(1006, "notebook", "hardware", 1300, 5),
   new Item(1007, "netbook", "hardware", 700, 7),
 ];
-
-function searchById() {
-  let itemId = Number(prompt("Enter an ID between 1000 and 1007"));
+function buscarId() {
+  let itemId = Number(prompt("Ingrese un ID entre 1000 y 1007"));
   while (itemId < 1000 || itemId > 1007 || isNaN(itemId)) {
-    itemId = Number(prompt("Enter a valid ID between 1000 and 1007"));
+    itemId = Number(prompt("Ingrese un ID entre 1000 y 1007"));
   }
-  const filteredItems = item.filter(item => item.id === itemId);
-  while (filteredItems.length === 0) {
-    itemId = Number(prompt("ID not found. Enter an ID between 1000 and 1007"));
-    filteredItems = item.filter(item => item.id === itemId);
+  let itemFiltrado = item.filter(item => item.id === itemId);
+  while (itemFiltrado.length === 0) {
+    itemId = Number(prompt("ID no encontrado. Ingrese un ID entre 1000 y 1007"));
+    itemFiltrado = item.filter(item => item.id === itemId);
   }
-  return filteredItems;
+  return itemFiltrado;
 }
-
-function searchByName() {
-  let itemName = prompt("Enter the name of the product: \n- Monitor \n- Keyboard \n- Mouse \n- Windows11 \n- Excel \n- Adobe pack \n- Notebook \n- Netbook").toLowerCase();
-  while (!["monitor", "keyboard", "mouse", "windows11", "excel", "adobe pack", "notebook", "netbook"].includes(itemName)) {
-    itemName = prompt("Enter a valid product name: \n- Monitor \n- Keyboard \n- Mouse \n- Windows11 \n- Excel \n- Adobe pack \n- Notebook \n- Netbook").toLowerCase();
+function buscarNombre() {
+  let itemName = prompt("Ingrese el nombre de un producto: \n- Monitor \n- Teclado \n- Mouse \n- Windows11 \n- Excel \n- Paquete de Adobe \n- Notebook \n- Netbook").toLowerCase();
+  while (!["monitor", "teclado", "mouse", "windows11", "excel", "paquete de adobe", "notebook", "netbook"].includes(itemName)) {
+    itemName = prompt("Ingrese un nombre de producto válido: \n- Monitor \n- Teclado \n- Mouse \n- Windows11 \n- Excel \n- Paquete de Adobe \n- Notebook \n- Netbook").toLowerCase();
   }
-  const filteredItems = item.filter(item => item.nombre === itemName);
-  while (filteredItems.length === 0) {
-    itemName = prompt("No product found with the entered name. Enter a valid name: \n- Monitor \n- Keyboard \n- Mouse \n- Windows11 \n- Excel \n- Adobe pack \n- Notebook \n- Netbook").toLowerCase();
-    filteredItems = item.filter(item => item.nombre === itemName);
+  let itemFiltrado = item.filter(item => item.nombre === itemName);
+  while (itemFiltrado.length === 0) {
+    itemName = prompt("Nombre no encontrado. Ingrese el nombre de un producto: \n- Monitor \n- Teclado \n- Mouse \n- Windows11 \n- Excel \n- Paquete de Adobe \n- Notebook \n- Netbook").toLowerCase();
+    itemFiltrado = item.filter(item => item.nombre === itemName);
   }
-  return filteredItems;
+  return itemFiltrado;
 }
-
-function searchByCategory() {
-  let itemCategory = prompt("Enter one of the following categories: \n- Periferico \n- Software \n- Hardware").toLowerCase();
-  while (!["periferico", "software", "hardware"].includes(itemCategory)) {
-    itemCategory = prompt("Enter a valid category from the following: \n- Periferico \n- Software \n- Hardware").toLowerCase();
+function buscarCategoria() {
+  let itemCat = prompt("Ingrese una de las siguientes categorías: \n- Periferico \n- Software \n- Hardware").toLowerCase();
+  while (!["periferico", "software", "hardware"].includes(itemCat)) {
+    itemCat = prompt("Ingrese una categoría válida de las siguientes: \n- Periferico \n- Software \n- Hardware").toLowerCase();
   }
-  const filteredItems = item.filter(item => item.categoria === itemCategory);
-  while (filteredItems.length === 0) {
-    itemCategory = prompt("No category matches the entered value. Enter a valid category from the following: \n- Periferico \n- Software \n- Hardware").toLowerCase();
-    filteredItems = item.filter(item => item.categoria === itemCategory);
+  let itemsFiltrados = item.filter(item => item.categoria === itemCat);
+  while (itemsFiltrados.length === 0) {
+    itemCat = prompt("No hay categorías que coincidan con el valor ingresado. Ingrese una categoría válida de las siguientes: \n- Periferico \n- Software \n- Hardware").toLowerCase();
+    itemsFiltrados = item.filter(item => item.categoria === itemCat);
   }
-  return filteredItems;
+  return itemsFiltrados;
 }
-
-function searchByPrice() {
-  let itemPrice = Number(prompt("Enter your budget"));
+function buscarPrecio() {
+  let itemPrice = Number(prompt("Ingrese su presupuesto"));
   while (isNaN(itemPrice) || itemPrice < 0) {
-    itemPrice = Number(prompt("Enter a budget greater than 0"));
+    itemPrice = Number(prompt("Ingrese un presupuesto mayor a 0"));
   }
-  const filteredItems = item.filter(item => item.precio <= itemPrice);
-  while (filteredItems.length === 0) {
-    itemPrice = Number(prompt("No products fit within your budget. Please enter a budget greater than $50"));
-    filteredItems = item.filter(item => item.precio <= itemPrice);
+  let itemsFiltrados = item.filter(item => item.precio <= itemPrice);
+  while (itemsFiltrados.length === 0) {
+    itemPrice = Number(prompt("No hay productos que se ajusten a su presupuesto. Por favor, ingrese un presupuesto mayor a $50"));
+    itemsFiltrados = item.filter(item => item.precio <= itemPrice);
   }
-  return filteredItems;
+  return itemsFiltrados;
 }
-
-function adminAccess() {
-  let adminAction = prompt("Indicate your need: Stock / Reorder").toLowerCase();
-  while (adminAction !== "exit") {
-    if (adminAction === "stock") {
-      const sortedStock = item.sort((a, b) => a.existencias - b.existencias);
-      sortedStock.forEach(item => {
-        const itemDetails = document.createElement("p");
-        itemDetails.textContent = `
+function accesoAdmin() {
+  let accionAdm = prompt("Indique su necesidad: Stock / Repo").toLowerCase();
+  while (accionAdm !== "salir") {
+    if (accionAdm === "stock") {
+      const stockOrdenado = item.sort((a, b) => a.existencias - b.existencias);
+      stockOrdenado.forEach(item => {
+        const listaItem = document.createElement("p");
+        listaItem.textContent =  `
           id: ${item.id}
           nombre: ${item.nombre}
-          categoria: ${item.categoria}
+          categoría: ${item.categoria}
           precio: $${item.precio}
           stock: ${item.existencias}
-        `;
-        inventoryElement.appendChild(itemDetails);
+        ` ;
+        inventoryElement.appendChild(listaItem);
       });
-    } else if (adminAction === "reorder") {
-      const sortedReorder = item.sort((a, b) => b.existencias - a.existencias);
-      sortedReorder.forEach(item => {
-        const itemDetails = document.createElement("p");
-        itemDetails.textContent = `
+    } else if (accionAdm === "repo") {
+      const repOrdenado = item.sort((a, b) => b.existencias - a.existencias);
+      repOrdenado.forEach(item => {
+        const listaItem = document.createElement("p");
+        listaItem.textContent =  `
           id: ${item.id}
           nombre: ${item.nombre}
-          categoria: ${item.categoria}
+          categoría: ${item.categoria}
           precio: $${item.precio}
           stock: ${item.existencias}
-        `;
-        inventoryElement.appendChild(itemDetails);
+        ` ;
+        inventoryElement.appendChild(listaItems);
       });
     } else {
-      alert("Indicate your need: Stock / Reorder or Exit to finish");
+      alert("Indique su necesidad: Stock / Reposicion o Salir para finalizar");
     }
-    adminAction = prompt("Indicate your need: Stock / Reorder or Exit to finish").toLowerCase();
+    accionAdm = prompt("Indique su necesidad: Stock / Reposicion o Salir para finalizar").toLowerCase();
   }
 }
-
 const inventoryElement = document.getElementById("inventory");
-
-alert("PreEntrega2 Naranjo Federico: Inventory Stock Control");
-let searchOption;
-let filteredItems = [];
-
+alert("PreEntrega2 Naranjo Federico: Control de Stock de Inventario");
+let opcionBusqueda;
+let itemsFiltrados = [];
 do {
-  searchOption = prompt("Enter one of the following options: \n- ID \n- Name \n- Category \n- Price \n- Admin (employee access)").toLowerCase();
-} while (searchOption !== "id" && searchOption !== "name" && searchOption !== "category" && searchOption !== "price" && searchOption !== "admin");
-
-switch (searchOption) {
+  opcionBusqueda = prompt("Ingrese una de las siguientes opciones: \n- ID \n- Nombre \n- Categoría \n- Precio \n- Admin (acceso de empleado)").toLowerCase();
+} while (opcionBusqueda !== "id" && opcionBusqueda !== "nombre" && opcionBusqueda !== "categoria" && opcionBusqueda !== "precio" && opcionBusqueda !== "admin");
+switch (opcionBusqueda) {
   case "id":
-    filteredItems = searchById();
+    itemsFiltrados = buscarId();
     break;
-  case "name":
-    filteredItems = searchByName();
+  case "nombre":
+    itemsFiltrados = buscarNombre();
     break;
-  case "category":
-    filteredItems = searchByCategory();
+  case "categoria":
+    itemsFiltrados = buscarCategoria();
     break;
-  case "price":
-    filteredItems = searchByPrice();
+  case "precio":
+    itemsFiltrados = buscarPrecio();
     break;
   case "admin":
-    adminAccess();
+    accesoAdmin();
     break;
   default:
-    alert("Enter one of the following options: \n- ID \n- Name \n- Category \n- Price \n- Admin (employee access)").toLowerCase();
-    while (searchOption !== "id" && searchOption !== "name" && searchOption !== "category" && searchOption !== "price" && searchOption !== "admin") {
-      searchOption = prompt("Enter one of the following options: \n- ID \n- Name \n- Category \n- Price \n- Admin (employee access)").toLowerCase();
+    alert("Ingrese una de las siguientes opciones: \n- ID \n- Nombre \n- Categoría \n- Precio \n- Admin (acceso de empleado)").toLowerCase();
+    while (opcionBusqueda !== "id" && opcionBusqueda !== "nombre" && opcionBusqueda !== "categoría" && opcionBusqueda !== "precio" && opcionBusqueda !== "admin") {
+      opcionBusqueda = prompt("Ingrese una de las siguientes opciones: \n- ID \n- Nombre \n- Categoría \n- Precio \n- Admin (acceso de empleado)").toLowerCase();
     }
     break;
 }
-
-if (filteredItems.length > 0) {
-  filteredItems.forEach(item => {
-    const itemDetails = document.createElement("p");
-    itemDetails.textContent = `
+if (itemsFiltrados.length > 0) {
+  itemsFiltrados.forEach(item => {
+    const detallesItem = document.createElement("p");
+    detallesItem.textContent =  `
       id: ${item.id}
       nombre: ${item.nombre}
-      categoria: ${item.categoria}
+      categoría: ${item.categoria}
       precio: $${item.precio}
       stock: ${item.existencias}
-    `;
-    inventoryElement.appendChild(itemDetails);
+    ` ;
+    inventoryElement.appendChild(detallesItem);
   });
 }
