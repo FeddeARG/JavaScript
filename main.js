3f92b926fdf01c0fc592bc303fd08f3082cc3bd7
FeddeARG <federicoanaranjo@gmail.com>
2023-11-01 16:46:12 -0300

    PFNaranjo1.0

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+localStorage.clear();
+
 class Item {
   constructor(id, nombre, categoria, precio, existencias) {
     this.id = id;
@@ -28,6 +30,7 @@ if (storedItemArray) {
     new Item(1005, "paquete de adobe", "software", 100, 70),
     new Item(1006, "notebook", "hardware", 1300, 5),
     new Item(1007, "netbook", "hardware", 700, 7),
+    new Item(1008, "Allin1", "hardware", 500, 4),
   ];
 
   // Guardamos en el localStorage
@@ -35,47 +38,47 @@ if (storedItemArray) {
 }
 
 const inventoryElement = document.getElementById("inventory");
-const busquedaOpcElement = document.getElementById("busquedaOpc");
+const searchOptElement = document.getElementById("searchOpt");
 const inputContainerElement = document.getElementById("inputContainer");
 const busquedaButton = document.getElementById("busquedaButton");
 
-busquedaOpcElement.addEventListener("change", handlebusquedaOpcChange);
+searchOptElement.addEventListener("change", handlesearchOptChange);
 busquedaButton.addEventListener("click", handlebusquedaButtonClick);
 
-function handlebusquedaOpcChange() {
-  const busquedaOpc = busquedaOpcElement.value;
+function handlesearchOptChange() {
+  const searchOpt = searchOptElement.value;
   inputContainerElement.innerHTML = "";
 
-  switch (busquedaOpc) {
-    
+  switch (searchOpt) {
+
     case "id":
       inputContainerElement.innerHTML = `
-        <label for="itemId">Ingrese ID (1000-1007):</label>
+        <label for="itemId">Ingrese un ID entre (1000-${item[item.length -1].id}):</label>
         <input type="number" id="itemId" min="1000" max="1007">
       `;
       break;
-    
+
     case "nombre":
       inputContainerElement.innerHTML = `
-        <label for="itemName">Ingrese nombre de producto:</label>
+        <label for="itemName">Ingrese el nombre de un producto:</label>
         <input type="text" id="itemName">
       `;
       break;
-    
+
     case "categoria":
       inputContainerElement.innerHTML = `
-        <label for="itemCategory">Ingrese categoría:</label>
+        <label for="itemCategory">Ingrese una categoría:</label>
         <input type="text" id="itemCategory">
       `;
       break;
-    
+
     case "precio":
       inputContainerElement.innerHTML = `
-        <label for="itemPrice">Ingrese presupuesto:</label>
+        <label for="itemPrice">Ingrese un presupuesto:</label>
         <input type="number" id="itemPrice" min="0">
       `;
       break;
-    
+
     case "admin":
       inputContainerElement.innerHTML = `
         <label for="adminAction">Selección de Administrador:</label>
@@ -85,80 +88,157 @@ function handlebusquedaOpcChange() {
         </select>
       `;
       break;
-    
+
     default:
       break;
   }
 }
 
 function handlebusquedaButtonClick() {
-  const busquedaOpc = busquedaOpcElement.value;
+  const searchOpt = searchOptElement.value;
+  const menorPrecio = item.reduce((min, currentItem) => {
+    return currentItem.precio < min ? currentItem.precio : min;
+  }, item[0].precio);
+  let ultimoID = item[item.length-1].id;
   let itemFiltrado = [];
+  let error = "";
+  
+  switch (searchOpt) {
 
-  switch (busquedaOpc) {
-    
     case "id":
+
       const itemId = Number(document.getElementById("itemId").value);
-      itemFiltrado = item.filter((item) => item.id === itemId);
+      if (itemId >= 1000 && itemId <= ultimoID) {
+        itemFiltrado = item.filter((item) => item.id === itemId);
+        if (!itemFiltrado.length) {
+          error = "No hay coincidencias con el ID ingresado.";
+        }
+      } else {
+        error = "Por favor, ingrese un ID de producto.";
+      }
       break;
-    
+
     case "nombre":
+
       const itemName = document.getElementById("itemName").value.toLowerCase();
-      itemFiltrado = item.filter((item) => item.nombre === itemName);
+      if (itemName.trim() !== "") {
+        itemFiltrado = item.filter((item) => item.nombre === itemName);
+        if (!itemFiltrado.length) {
+          error = "No hay coincidencias con el nombre ingresado.";
+        }
+      } else {
+        error = "Por favor, ingrese el nombre de un producto.";
+      }
       break;
-    
+
     case "categoria":
+
       const itemCategory = document.getElementById("itemCategory").value.toLowerCase();
-      itemFiltrado = item.filter((item) => item.categoria === itemCategory);
+      if (itemCategory.trim() !== "") {
+        itemFiltrado = item.filter((item) => item.categoria === itemCategory);
+        if (!itemFiltrado.length) {
+          error = "No hay coincidencias con la categoría ingresada.";
+        }
+      } else {
+        error = "Por favor, ingrese una categoría de productos.";
+      }
       break;
-    
+
     case "precio":
+
       const itemPrice = Number(document.getElementById("itemPrice").value);
-      itemFiltrado = item.filter((item) => item.precio <= itemPrice);
+      if (!isNaN(itemPrice) && itemPrice >= 0) {
+        itemFiltrado = item.filter((item) => item.precio <= itemPrice);
+        if (!itemFiltrado.length) {
+          error = "No hay coincidencias dentro del rango establecido.";
+        }
+      } else {
+        error = "Por favor, ingrese un presupuesto mayor a " + menorPrecio ;
+      }
       break;
-    
+
     case "admin":
+
       handleAdminAction();
       return;
-    
+
     default:
       break;
   }
 
-  displayItems(itemFiltrado);
+  if (error !== "") {
+    const errorElement = document.createElement("p");
+    errorElement.textContent = error;
+    inventoryElement.innerHTML = "";
+    inventoryElement.append(errorElement);
+  } else {
+    displayItems(itemFiltrado);
+  }
 }
 
 function handleAdminAction() {
   const adminAction = document.getElementById("adminAction").value;
 
   switch (adminAction) {
-    
+
     case "stock":
       const stockOrdenado = item.sort((a, b) => a.existencias - b.existencias);
       displayItems(stockOrdenado);
       break;
-  
+
     case "reposicion":
       const repOrdenado = item.sort((a, b) => b.existencias - a.existencias);
       displayItems(repOrdenado);
       break;
-    
+
     default:
       break;
   }
 }
 
+function displayArrayInfo() {
+  const arrayContainer = document.getElementById("arrayContainer");
+  let formattedArrayInfo = "";
+
+  item.forEach(function(item) {
+    formattedArrayInfo += "ID: " + item.id + "\n";
+    formattedArrayInfo += "Nombre: " + item.nombre + "\n";
+    formattedArrayInfo += "Categoría: " + item.categoria + "\n";
+    formattedArrayInfo += "Precio: $" + item.precio + "\n";
+    formattedArrayInfo += "Existencias: " + item.existencias + "\n\n";
+  });
+
+  arrayContainer.textContent = formattedArrayInfo;
+}
+
+displayArrayInfo();
+
 function displayItems(items) {
   inventoryElement.innerHTML = "";
   items.forEach((item) => {
-    const detallesItem = document.createElement("p");
-    detallesItem.textContent = `
-      id: ${item.id}
-      nombre: ${item.nombre}
-      categoría: ${item.categoria}
-      precio: $${item.precio}
-      stock: ${item.existencias}
-    `;
-    inventoryElement.append(detallesItem);
+    const card = document.createElement("div");
+    card.classList.add("card");
+
+    const idElement = document.createElement("p");
+    idElement.textContent = `ID: ${item.id}`;
+    card.append(idElement);
+
+    const nombreElement = document.createElement("p");
+    nombreElement.textContent = `Nombre: ${item.nombre}`;
+    card.append(nombreElement);
+
+    const categoriaElement = document.createElement("p");
+    categoriaElement.textContent = `Categoría: ${item.categoria}`;
+    card.append(categoriaElement);
+
+    const precioElement = document.createElement("p");
+    precioElement.textContent = `Precio: $${item.precio}`;
+    card.append(precioElement);
+
+    const existenciasElement = document.createElement("p");
+    existenciasElement.textContent = `Existencias: ${item.existencias}`;
+    card.append(existenciasElement);
+
+    inventoryElement.append(card);
   });
 }
